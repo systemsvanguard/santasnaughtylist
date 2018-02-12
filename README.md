2794eee7098e575fa39b6865ae47bb8a56536f5c
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2018-02-12 03:19:16 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Santa's Naughty List
+
+An **Angular.js** and W3.js powered **List App** application by a coder that watches far too many political late night comedy shows.
+
+![Santa's Naughty List](http://ryanhunter.org/portfolio/santasnaughtylist/thumb_santasnaughtylist.jpg)
+
+---
+
+## Features
+- Angular.js framework with filtering
+- Bootstrap 3
+- Fontastic.me custom generated vector icons
+- Google Fonts
+
+
+## License
+
+This project is licensed under the terms of the **MIT** license.
---
+++ santasnaughtylist/santasnaughtylist.js
@@ -0,0 +1,19 @@
+/* Santa's Naughty List */
+/* Names of people that deserve coal in their Christmas stockings.  */
+var app = angular.module("naughtyList", []); 
+app.controller("naughtyCtrl", function($scope) {
+    $scope.products = ["Donald Trump", "Hillary Clinton", "Vladimir Putin", "Kim Jong-un", "The CEO of my greedy Cell Phone company"];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "This person is already on the Naughty List.  While they might deserve to be there multiple times, it's against the rules!";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.products.splice(x, 1);
+    }
+});
